2cb0dfbd9ca288df79b74aa2da2f5a3a0a3bd69c
joaquim8gomes <joaquimgomes2468@gmail.com>
2026-04-29 23:17:13 -0300

    correcao de bug: server.js entrando em lopping infinito. Agora localhost entra direto no arquivo login.html e é direcionado para index.html

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  fs.readFile('index.html', (err, data) => {
+  fs.readFile('login.html', (err, data) => {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(data);
     res.end();
