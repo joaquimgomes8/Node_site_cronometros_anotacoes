41e5056e676c261c5a91d5859b1ad5a79e74dfd0
joaquim8gomes <joaquimgomes2468@gmail.com>
2026-05-05 22:55:58 -0300

    correção de comentarios no codigo

--- server.js
+++ server.js
@@ -3,10 +3,8 @@ const fs = require('fs');
 const path = require('path');
 
 const server = http.createServer((req, res) => {
-  // Define qual arquivo carregar com base na URL
   let filePath = req.url === '/' ? 'login.html' : req.url.substring(1);
   
-  // Proteção básica para evitar que o usuário saia da pasta do projeto
   filePath = path.join(__dirname, filePath);
 
   fs.readFile(filePath, (err, data) => {
@@ -17,7 +15,6 @@ const server = http.createServer((req, res) => {
       return;
     }
 
-    // Define o Content-Type correto baseado na extensão
     const ext = path.extname(filePath);
     let contentType = 'text/html';
     if (ext === '.js') contentType = 'text/javascript';
